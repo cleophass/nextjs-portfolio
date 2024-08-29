b006fc8bc45838ce4e58b3b545ddcad7ef2725b2
Cléophas Fournier <cleophasfournier@mbp-de-cleophas.home>
2024-08-29 15:27:41 +0200

    add loging

--- src/app/api/send-email/route.ts
+++ src/app/api/send-email/route.ts
@@ -1,54 +1,35 @@
-import { NextResponse } from 'next/server';
-import nodemailer from 'nodemailer';
+    import { NextResponse } from 'next/server';
+    import nodemailer from 'nodemailer';
 
-// Fonction pour gérer les requêtes POST
-export async function POST(req: Request) {
-  try {
-    
+    // Fonction pour gérer les requêtes POST
+    export async function POST(req: Request) {
+    try {
+        const { to, subject, text } = await req.json();
 
-    // Parse the incoming JSON data
-    const { email, subject, message } = await req.json();
-    
-    // Log the received data
-    
+        if (!to || !subject || !text) {
+        return NextResponse.json({ message: 'Missing required fields' }, { status: 400 });
+        }
 
-    // Check if required fields are present
-    if (!email || !subject || !message) {
-      
-      return NextResponse.json({ message: 'Missing required fields' }, { status: 400 });
+        const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS,
+        },
+        });
+
+        const mailOptions = {
+        from: process.env.EMAIL_USER,
+        to,
+        subject,
+        text,
+        };
+
+        await transporter.sendMail(mailOptions);
+
+        return NextResponse.json({ message: 'Email sent successfully' }, { status: 200 });
+    } catch (error) {
+        console.error('Error sending email:', error);
+        return NextResponse.json({ message: 'Error sending email' }, { status: 500 });
     }
-
-    // Create the transporter object for Nodemailer
-    const transporter = nodemailer.createTransport({
-      service: 'gmail',
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
-
-    // Log transporter configuration
-   
-
-    // Email options
-    const mailOptions = {
-      from: process.env.EMAIL_USER,
-      to: 'cleophas.fournier@gmail.com',  // Destinataire fixe
-      subject,
-      text: `Nouveau mail envoyé via le portfolio par : ${email}\n\nVoici le message :\n${message}`,
-    };
-
-    // Log email options
-    
-
-    // Send the email
-    await transporter.sendMail(mailOptions);
-
-    
-
-    return NextResponse.json({ message: 'Email sent successfully' }, { status: 200 });
-  } catch (error) {
-    
-    return NextResponse.json({ message: 'Error sending email' }, { status: 500 });
-  }
-}
+    }
