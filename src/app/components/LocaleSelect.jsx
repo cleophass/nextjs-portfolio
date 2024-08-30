31717824755ff0899a7eeffc800f3d19fd82031d
Cléophas Fournier <cleophasfournier@mbp-de-cleophas.home>
2024-08-30 17:25:22 +0200

    final push

--- src/app/components/LocaleSelect.jsx
+++ src/app/components/LocaleSelect.jsx
@@ -1,18 +1,15 @@
 import React from 'react';
+import { useCurrentLocale,useChangeLocale } from '../../locales/client';
 
-type Locale = "fr" | "en";
 
-interface LocaleSelectProps {
-  locale: Locale;
-  changeLocale: (newLocale: Locale) => void;
-}
-
-const LocaleSelect: React.FC<LocaleSelectProps> = ({ locale, changeLocale }) => {
+const LocaleSelect= () => {
+  const locale = useCurrentLocale();
+  const changeLocale = useChangeLocale();
   return (
     <select
       className="bg-transparent border-gray-50 rounded-md px-2 py-1 text-2xl focus:outline-none"
       value={locale}
-      onChange={(e) => changeLocale(e.target.value as Locale)}
+      onChange={(e) => changeLocale(e.target.value )}
     >
       <option value="en">🇬🇧</option>
       <option value="fr">🇫🇷</option>
--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -4,7 +4,7 @@ import React, { useState } from "react";
 import NavLink from "./NavLink";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import MenuOverlay from "./MenuOverlay";
-import LocaleSelect from "./LocaleSelect";  // Assure-toi que le chemin est correct
+import LocaleSelect from "./LocaleSelect";
 
 const navLinks = [
   {
@@ -23,49 +23,44 @@ const navLinks = [
 
 const Navbar = () => {
   const [navbarOpen, setNavbarOpen] = useState(false);
-  const [locale, setLocale] = useState("en");  // Définir l'état de la langue
-
-  const changeLocale = (newLocale) => {
-    setLocale(newLocale);
-    // Ajouter ici la logique pour changer la langue dans l'application, si nécessaire
-  };
 
   return (
     <nav className="fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-20 bg-[#121212] bg-opacity-100">
       <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
         
-        <div className="mobile-menu block md:hidden">
-          {!navbarOpen ? (
-            <button
-              onClick={() => setNavbarOpen(true)}
-              className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
-            >
-              <Bars3Icon className="h-5 w-5" />
-            </button>
-          ) : (
-            <button
-              onClick={() => setNavbarOpen(false)}
-              className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
-            >
+        {/* Mobile Menu Button */}
+        <div className="mobile-menu flex items-center w-full md:hidden justify-between">
+          <button
+            onClick={() => setNavbarOpen(!navbarOpen)}
+            className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
+          >
+            {navbarOpen ? (
               <XMarkIcon className="h-5 w-5" />
-            </button>
-          )}
-        </div>
-        <div className="menu hidden md:block md:w-auto" id="navbar">
-          <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0">
-            {navLinks.map((link, index) => (
-              <li key={index}>
-                <NavLink href={link.path} title={link.title} />
-              </li>
-            ))}
-          </ul>
-        </div>
-        
-        {/* Ajoute le composant LocaleSelect ici */}
-        <div className="ml-4">
-          <LocaleSelect locale={locale} changeLocale={changeLocale} />
+            ) : (
+              <Bars3Icon className="h-5 w-5" />
+            )}
+          </button>
+          <LocaleSelect />
         </div>
 
+        {/* Desktop and Mobile Navigation and Locale Select */}
+        <div className="flex w-full items-center justify-between">
+          {/* Desktop Navigation */}
+          <div className="hidden md:flex md:space-x-8">
+            <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0">
+              {navLinks.map((link, index) => (
+                <li key={index}>
+                  <NavLink href={link.path} title={link.title} />
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          {/* Locale Select on Desktop */}
+          <div className="hidden md:flex items-center ml-auto">
+            <LocaleSelect />
+          </div>
+        </div>
       </div>
       {navbarOpen ? <MenuOverlay links={navLinks} /> : null}
     </nav>
