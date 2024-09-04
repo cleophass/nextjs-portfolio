c72c42498a829cd94e6feb17cb282817e1db1341
Cléophas Fournier <cleophasfournier@mbp-de-cleophas.home>
2024-09-04 13:38:26 +0200

    next-international fix

--- src/app/components/CvButton.tsx
+++ src/app/components/CvButton.tsx
@@ -10,7 +10,7 @@ const DownloadButton = () => {
       className="flex items-center justify-center px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-400 to-blue-600 hover:bg-slate-800 text-white mt-3"
     >
       <button className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2 w-full">
-        {t("cv")}
+        {t("cv",{ count: 1 })}
       </button>
     </a>
   );
